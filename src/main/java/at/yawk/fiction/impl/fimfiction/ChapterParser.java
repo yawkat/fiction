1e08d4f2b3c82b57e381b56c39758c0676188d4b
Jonas Konrad <me@yawk.at>
2015-09-12 20:23:57 +0200

    Don't try to parse chapter title, this apparently isn't portable

--- src/main/java/at/yawk/fiction/impl/fimfiction/ChapterParser.java
+++ src/main/java/at/yawk/fiction/impl/fimfiction/ChapterParser.java
@@ -15,7 +15,6 @@ class ChapterParser extends PageParser<FimChapter> {
 
     @Override
     protected void parse(Element root, FimChapter target) throws Exception {
-        target.setName(root.select("h3").first().text());
         HtmlText html = new HtmlText();
         html.setHtml(root.select("body").first().html());
         target.setText(html);
